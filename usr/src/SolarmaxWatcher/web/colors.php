038b082eae659598928e8ff90864a7dc1174b497
lassowski <lassowski@963ea8ed-a92e-4dc3-8da7-d6a7ac228f3d>
2010-10-31 11:14:58 +0000

    remove old /usr and subdirectories

--- usr/src/SolarmaxWatcher/web/colors.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $white = imagecolorallocate($image, 255, 255, 255);
-   $gray = imagecolorallocate($image, 191, 191, 191);
-   $black = imagecolorallocate($image, 0, 0, 0);
-   $green = imagecolorallocatealpha($image, 0, 196, 64, 20);   
-   $blue = imagecolorallocate($image, 0, 0, 255);
-   $red = imagecolorallocate($image, 255, 0, 0);
-   $yellow = imagecolorallocate($image, 227, 225, 27);
-?>
