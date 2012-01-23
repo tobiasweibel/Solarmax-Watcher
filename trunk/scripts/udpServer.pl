2ed90ad396dfb8bb7c0a5a336f91170a8f92d2cf
sleepProgger <sleepProgger@963ea8ed-a92e-4dc3-8da7-d6a7ac228f3d>
2012-01-23 00:48:05 +0000

    simple perl udp listener

---
+++ trunk/scripts/udpServer.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+#	Simple perl udp listener which print the KDY and the PAC values provided by the smw-logger
+#	Source base from: http://www.thegeekstuff.com/2010/07/perl-tcp-udp-socket-programming/
+#	This is my first perl script. So please forgive me ;)
+#	Author:		SleepProgger
+#
+use IO::Socket::INET;
+# flush after every write
+$| = 1;
+
+my $regex = "{[^}]*KDY=([0-9]+)[^}]*PAC=([0-9]+)[^}]*}";
+my $localPort = "4950";
+my ($socket,$received_data);
+my ($peeraddress,$peerport);
+if(@ARGV > 0 && $ARGV[0] =~ m/\-?(\?|help|h)/){
+	printf "USAGE: udpServer.pl [port [regex]]\n";
+	printf "Regex have to provide \$1 (KDY) and \$2 (PAC)\n";
+	exit 0;
+}
+if(@ARGV > 0){
+	$localPort = $ARGV[0];
+	printf "local port set to $localPort\n";
+}
+if(@ARGV > 1){
+	$regex = $ARGV[1];
+	printf "regex set to $regex\n";
+}
+
+
+#  we call IO::Socket::INET->new() to create the UDP Socket and bound
+$socket = new IO::Socket::INET (
+LocalPort => $localPort,
+Proto => 'udp',
+) or die "ERROR in Socket Creation : $!\n";
+while(1){
+	# read operation on the socket
+	$socket->recv($recieved_data,1024);
+	$recieved_data =~ s/($regex)/KDY: $2 kWh\nPAC: $3/;
+	system "clear";
+	print $recieved_data;
+}
+$socket->close();
